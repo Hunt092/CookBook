a855d9357ac6b08e686c5d5b56c54e7b82b75bc0
Yash Chavan <hunters.hunt92@gmail.com>
2021-04-09 22:55:01 +0530

    Abstract scss created, directory structure made Todo List Made

---
+++ src/Component/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio aut, enim, excepturi laborum consequuntur voluptatum, tenetur porro deleniti ea ipsam eum nulla id quaerat nobis quis quo similique incidunt. Sapiente.
+            </p>
+            {/* <h2>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio aut, enim, excepturi laborum consequuntur voluptatum, tenetur porro deleniti ea ipsam eum nulla id quaerat nobis quis quo similique incidunt. Sapiente.
+            </h2>
+            <h3>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio aut, enim, excepturi laborum consequuntur voluptatum, tenetur porro deleniti ea ipsam eum nulla id quaerat nobis quis quo similique incidunt. Sapiente.
+            </h3> */}
+        </div>
+    )
+}
+
+export default Header
