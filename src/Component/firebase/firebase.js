4caec7e85e1f9d8e23f9097e31807fa10c659240
Rohit-Kavitake <kavitakerohit2001@gmail.com>
2021-04-13 15:41:21 +0530

    added function for image upload

--- src/Component/firebase/firebase.js
+++ src/Component/firebase/firebase.js
@@ -1,5 +1,6 @@
 // <!-- The core Firebase JS SDK is always required and must be listed first -->
 // <script src="https://www.gstatic.com/firebasejs/8.4.0/firebase-app.js"></script>
+// <script src="https://www.gstatic.com/firebasejs/8.4.0/firebase-firestore.js"></script>
 
 // // <!-- TODO: Add SDKs for Firebase products that you want to use
 // //      https://firebase.google.com/docs/web/setup#available-libraries -->
@@ -82,6 +83,32 @@ const addData = (
 			console.log("Error : " + err);
 		});
 };
+
+
+// Saves a new message containing an image in Firebase.
+// This first saves the image in Firebase storage.
+function saveImageMessage(file) {
+  // Posts a new image as a message.
+  firebase.firestore().collection('messages').add({
+    imageUrl: LOADING_IMAGE_URL,
+  }).then(function(messageRef) {
+    // 2 - Upload the image to Cloud Storage.
+    var filePath = firebase.auth().currentUser.uid + '/' + messageRef.id + '/' + file.name;
+    return firebase.storage().ref(filePath).put(file).then(function(fileSnapshot) {
+      // 3 - Generate a public URL for the file.
+      return fileSnapshot.ref.getDownloadURL().then((url) => {
+        // 4 - Update the chat message placeholder with the image's URL.
+        return messageRef.update({
+          imageUrl: url,
+          storageUri: fileSnapshot.metadata.fullPath
+        });
+      });
+    });
+  }).catch(function(error) {
+    console.error('There was an error uploading a file to Cloud Storage:', error);
+  });
+}
+
 
 export default getAllData;
 export default getSingleData;
